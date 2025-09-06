e515e1b083b165195955dc5580aee5fd34481fa6
cyberhell088 <cyberhell088@gmail.com>
2025-09-06 13:59:05 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+document.getElementById('driverBtn').addEventListener('click', function() {
+  alert('Driver page will open');
+  // Uncomment below to redirect to driver page
+  // window.location.href = 'driver.html';
+});
+
+document.getElementById('passengerBtn').addEventListener('click', function() {
+  alert('Passenger page will open');
+  // Uncomment below to redirect to passenger page
+  // window.location.href = 'passenger.html';
+});
