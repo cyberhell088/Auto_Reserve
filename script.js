da9e4932d281b53b6b8003070680b2f235068ab1
cyberhell088 <cyberhell088@gmail.com>
2025-09-06 19:53:12 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// script.js
 
-document.getElementById('driverBtn').addEventListener('click', function() {
-  alert('Welcome, Driver! You will be redirected to the driver page.');
-  // Redirect to driver page
-  // window.location.href = 'driver.html';
-});
-
-document.getElementById('passengerBtn').addEventListener('click', function() {
-  // Redirect to passenger signup page
-  window.location.href = 'passenger.html';
-});
